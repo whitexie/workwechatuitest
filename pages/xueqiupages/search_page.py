11a82920ffaed3e6821cb746fec0b22bcf0f1edf
xiebo <shieber@163.com>
2019-08-25 22:15:47 +0800

    添加退出搜索页方法

--- pages/xueqiupages/search_page.py
+++ pages/xueqiupages/search_page.py
@@ -7,6 +7,7 @@ class SearchPage:
     _search_input = (By.ID, 'search_input_text')
     _name = (By.ID, 'name')
     _search_prompt = (By.ID, 'fl_suggest')
+    _close_search = (By.ID, 'action_close')
 
     # search result
     _stock_data = (By.ID, 'add_attention')
@@ -21,6 +22,13 @@ class SearchPage:
     def __init__(self, driver: WebDriver):
         self.driver = driver
 
+    def canal_search(self):
+        """
+        退出搜索页面，返回上一页
+        :return:
+        """
+        self.driver.find_element(*self._close_search).click()
+
     def search_stock(self, content):
         el = self.driver.find_element(*self._search_input)
         el.send_keys(content)
@@ -29,10 +37,16 @@ class SearchPage:
         elements[0].click()
         return self
 
+    def _search_result_list(self):
+        return self.driver.find_elements(*self._stock_data)
+
     def follow_stock_by_index(self, index):
         self.driver.find_elements(*self._follow)[index].click()
         self.driver.find_element(*self._md).click()
         return self.driver.find_elements(*self._followed)[index].text
 
     def follow_status(self):
-        return self.driver.find_elements(*self._stock_data)[0].find_element(*self._follow).text
+        return self._search_result_list()[0].find_element(*self._follow).text
+
+    def followed_status(self):
+        return self._search_result_list()[0].find_element(*self._followed).text
