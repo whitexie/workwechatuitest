5912b5f149249e909a9c7d9b96d1e61d56d98956
shieber <shieber@163.com>
2019-08-27 15:08:47 +0800

    add remotedriver

--- baseframe/remotedriver.py
+++ baseframe/remotedriver.py
@@ -3,15 +3,13 @@ from appium.webdriver.webdriver import WebDriver
 
 
 def get_driver() -> WebDriver:
-
-    caps = {
-        'platformName': 'Android',
-        'platformVersion': '8.1.0',
-        'deviceName': '1d8ce5e',
-        'appPackage': 'com.xueqiu.android',
-        'appActivity': '.view.WelcomeActivityAlias',
-        'autoGrantPermissions': True
-    }
+    caps = dict()
+    caps['platformName'] = 'Android'
+    caps['platformVersion'] = '8.0.0'
+    caps['deviceName'] = 'DLQ0215C13000724'
+    caps['appPackage'] = 'com.xueqiu.android'
+    caps['appActivity'] = '.view.WelcomeActivityAlias'
+    caps['autoGrantPermissions'] = True
     driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_capabilities=caps)
     driver.implicitly_wait(15)
     return driver
