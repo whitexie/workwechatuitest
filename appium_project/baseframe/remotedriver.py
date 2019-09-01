ee26a7dabfe6790b98175f09016421c3d935c546
xiebo <shieber@163.com>
2019-09-01 22:53:53 +0800

    将机型存储在yaml文件进行读取

--- appium_project/baseframe/remotedriver.py
+++ appium_project/baseframe/remotedriver.py
@@ -1,15 +1,17 @@
+import yaml
 from appium import webdriver
 from appium.webdriver.webdriver import WebDriver
 
 
-def get_driver() -> WebDriver:
+def get_driver(config_path) -> WebDriver:
     caps = dict()
     caps['platformName'] = 'Android'
-    caps['platformVersion'] = '8.0.0'
-    caps['deviceName'] = 'DLQ0215C13000724'
     caps['appPackage'] = 'com.xueqiu.android'
     caps['appActivity'] = '.view.WelcomeActivityAlias'
     caps['autoGrantPermissions'] = True
+    with open(config_path, 'r') as data:
+        caps.update(yaml.load(data))
+    print(caps)
     driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_capabilities=caps)
     driver.implicitly_wait(15)
     return driver
