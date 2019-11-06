b37ba74e6e94ac55bf8748e1022c01b787cbb930
shieber <shieber@163.com>
2019-11-06 11:50:02 +0800

    添加allure.step

--- interface_po_test/api/department.py
+++ interface_po_test/api/department.py
@@ -1,4 +1,5 @@
 from interface_po_test.api.baseapi import BaseApi
+import allure
 
 
 class Department(BaseApi):
@@ -6,6 +7,7 @@ class Department(BaseApi):
     _search_list_uri = '/cgi-bin/department/list'
     _delete = '/cgi-bin/department/delete'
 
+    @allure.step('创建部门')
     def create(self, name, parentid=1, order=1000, id=None):
         headers = {
             'Content-Type': 'application/json; charset=utf-8'
@@ -21,8 +23,10 @@ class Department(BaseApi):
     def update(self):
         pass
 
+    @allure.step('获取部门列表')
     def search_list(self, depart_id=None):
         return self.get(self._search_list_uri, params={'id': depart_id})
 
+    @allure.step('删除部门')
     def delete(self, depart_id=None):
         return self.get(self._search_list_uri, params={'id': depart_id})
